9b670f9c5f6c587f9b21b45e02bf455a981ff276
Pansy <so.alfaro@duocuc.cl>
2023-10-30 23:38:21 -0300

    pop up
    aa

--- src/app/detalle/detalle.page.ts
+++ src/app/detalle/detalle.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router'; // Importa el Router de Angular
 
 @Component({
   selector: 'app-detalle',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalle.page.scss'],
 })
 export class DetallePage implements OnInit {
+  public alertButtons = ['OK'];
 
-  constructor() { }
+  constructor(private alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
   }
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Arriendo Realizado',
+      message: 'Se realizó el arriendo exitosamente',
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            // Utiliza el Router para la redirección
+            this.router.navigate(['/list']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 }
