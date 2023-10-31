8f79df547e020dcd735847b70d564aa09abf029b
se4arriagadaa <84482238+se4arriagadaa@users.noreply.github.com>
2023-10-31 00:48:21 -0300

    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 const routes: Routes = [
   {
     path: 'home',
